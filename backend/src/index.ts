c13926ba82fbac16d926a09d1df17e226255e341
rapoluprashanth <rapoluprashanth123@gmail.com>
2025-12-27 16:19:40 +0530

    connected backend to mongodb and update config files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,15 @@
-const a: string = "Hello, World!";
-console.log(a);
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/database.ts";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+connectDB();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
